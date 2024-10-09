f09dd0daecda4735fde8fe1c8cc81c5cc6c8b09d
ThanaphatSuporn <thanaphatsupornsecondary@gmail.com>
2024-10-09 16:12:07 +0700

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,6 @@
+local PooruiLib = loadstring(game:HttpsGet("https://raw.githubusercontent.com/ThanaphatSuporn/Poorui-Lib/refs/heads/main/poorui.lua"))
+local Window = Poorui:Create("Emtpy Hub")
+
+PoorUi:CreateButton("infnite yield", function ()
+    print("lying")
+end)
