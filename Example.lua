4358afe9e55c67370bc00478f6b428671fef6e8f
ThanaphatSuporn <thanaphatsupornsecondary@gmail.com>
2024-10-10 09:13:21 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local poorui = loadstring(game:HttpsGet("https://raw.githubusercontent.com/ThanaphatSuporn/Poorui-Lib/refs/heads/main/poorui.lua"))
+local poorui = loadstring(game:HttpGet"https://raw.githubusercontent.com/ThanaphatSuporn/Poorui-Lib/refs/heads/main/poorui.lua")()
 local Window = poorui:Create("Emtpy Hub")
 
 poorui:CreateButton("infnite yield", function ()
